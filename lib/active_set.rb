f183d0e0c924986067fbffc59382eba84b4c60ac
Stephen Margheim <margheim@mail.med.upenn.edu>
2017-09-09 14:55:14 -0400

    Use some ActiveSupport methods to clean things up a bit

--- lib/active_set.rb
+++ lib/active_set.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'active_set/version'
+require 'active_support/core_ext/object/blank'
 
 class ActiveSet
   include Enumerable
@@ -14,14 +15,14 @@ class ActiveSet
   end
 
   def filter(structure)
-    structure.reject { |_, value| value.nil? || value.empty? }
+    structure.reject { |_, value| value.blank? }
              .reduce(@set) do |set, (key, value)|
                set.select { |item| item.send(key) == value }
              end
   end
 
   def sort(structure)
-    structure.reject { |_, value| value.nil? || value.empty? }
+    structure.reject { |_, value| value.blank? }
              .reduce(@set) do |set, (key, value)|
                set.sort_by { |item| item.send(key) }
                   .tap     { |c| c.reverse! if value.to_s == 'desc' }
