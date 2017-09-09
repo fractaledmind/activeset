badb789e9d128a173af3edb10c6be31847f675e4
Stephen Margheim <margheim@mail.med.upenn.edu>
2017-09-09 14:14:55 -0400

    Add basic filtering functionality

--- lib/active_set.rb
+++ lib/active_set.rb
@@ -12,4 +12,11 @@ class ActiveSet
   def each(&block)
     @set.each(&block)
   end
+
+  def filter(structure)
+    structure.reject { |_, value| value.nil? || value.empty? }
+             .reduce(@set) do |set, (key, value)|
+               set.select { |item| item.send(key) == value }
+             end
+  end
 end
--- spec/activeset_spec.rb
+++ spec/activeset_spec.rb
@@ -1,13 +1,21 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require 'ostruct'
 
 RSpec.describe ActiveSet do
   it 'has a version number' do
     expect(ActiveSet::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe 'filtering' do
+    let(:foo) { OpenStruct.new(key: 'foo') }
+    let(:bar) { OpenStruct.new(key: 'bar') }
+    let(:baz) { OpenStruct.new(key: 'baz') }
+    let(:set) { [foo, bar, baz] }
+    let(:active_set) { ActiveSet.new set }
+
+    it { expect(active_set.filter(key: 'bar')).to eq [bar] }
+    it { expect(active_set.filter(key: 'val')).to eq [] }
   end
 end
