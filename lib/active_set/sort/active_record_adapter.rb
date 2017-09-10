25e7a54ec9743d9824423ae5009f724cae0b65d8
Stephen Margheim <margheim@mail.med.upenn.edu>
2017-09-09 23:19:30 -0400

    Add an ActiveRecordAdapter for the Sort processor

---
+++ lib/active_set/sort/active_record_adapter.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require_relative '../base_adapter'
+
+class ActiveSet
+  module Sort
+    class ActiveRecordAdapter < BaseAdapter
+      def process(set)
+        @set = set
+        return @set unless @set.respond_to? :to_sql
+        return @set unless attribute_is_field?
+
+        @set.order(@structure_path.attribute => @value)
+        @set.includes(@structure_path.to_h)
+            .references(@structure_path.to_h)
+            .merge(arel_operation)
+      end
+
+      private
+
+      def attribute_is_field?
+        return false unless attribute_model
+        attribute_model.attribute_names
+                       .include?(@structure_path.attribute)
+      end
+
+      def arel_operation
+        attribute_model.order(arel_column.lower.send(@value))
+      end
+
+      def attribute_model
+        @structure_path.to_a
+                       .reduce(@set) do |obj, assoc|
+                         obj.reflections[assoc.to_s]&.klass
+                       end
+      end
+
+      def arel_column
+        arel_table[@structure_path.attribute]
+      end
+
+      def arel_table
+        Arel::Table.new(attribute_model.table_name)
+      end
+    end
+  end
+end
