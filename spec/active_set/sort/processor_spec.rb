dc7032884f5bec41944796c7315b39bddd1bdc3c
Stephen Margheim <margheim@mail.med.upenn.edu>
2017-09-09 23:09:29 -0400

    Add tests for the Sort processor to work with an ActiveRecord::Relation set

--- spec/active_set/sort/processor_spec.rb
+++ spec/active_set/sort/processor_spec.rb
@@ -4,41 +4,84 @@ require 'spec_helper'
 require 'ostruct'
 
 RSpec.describe ActiveSet::Sort::Processor do
-  let(:foo) { OpenStruct.new(key: 'foo', association: OpenStruct.new(key: 'oof')) }
-  let(:bar) { OpenStruct.new(key: 'bar', association: OpenStruct.new(key: 'rab')) }
-  let(:baz) { OpenStruct.new(key: 'baz', association: OpenStruct.new(key: 'zab')) }
-  let(:set) { [foo, bar, baz] }
-
   let(:processor) { described_class.new(set, sort_structure) }
 
-  describe '#process' do
-    subject { processor.process }
+  context 'when set is simple Enumerable' do
+    let(:foo) { OpenStruct.new(key: 'foo', association: OpenStruct.new(key: 'oof')) }
+    let(:bar) { OpenStruct.new(key: 'bar', association: OpenStruct.new(key: 'rab')) }
+    let(:baz) { OpenStruct.new(key: 'baz', association: OpenStruct.new(key: 'zab')) }
+    let(:set) { [foo, bar, baz] }
 
-    context 'with attributes on the base resource' do
-      context 'when direction is ASC' do
-        let(:sort_structure) { { key: :asc } }
+    describe '#process' do
+      subject { processor.process }
 
-        it { should eq [bar, baz, foo] }
+      context 'with attributes on the base resource' do
+        context 'when direction is ASC' do
+          let(:sort_structure) { { key: :asc } }
+
+          it { should eq [bar, baz, foo] }
+        end
+
+        context 'when direction is DESC' do
+          let(:sort_structure) { { key: 'desc' } }
+
+          it { should eq [foo, baz, bar] }
+        end
       end
 
-      context 'when direction is DESC' do
-        let(:sort_structure) { { key: 'desc' } }
+      context 'with attributes on an associated resource' do
+        context 'when direction is ASC' do
+          let(:sort_structure) { { association: { key: 'asc' } } }
 
-        it { should eq [foo, baz, bar] }
+          it { should eq [foo, bar, baz] }
+        end
+
+        context 'when direction is DESC' do
+          let(:sort_structure) { { association: { key: :desc } } }
+
+          it { should eq [baz, bar, foo] }
+        end
       end
     end
+  end
 
-    context 'with attributes on an associated resource' do
-      context 'when direction is ASC' do
-        let(:sort_structure) { { association: { key: 'asc' } } }
+  context 'when set is ActiveRecord::Relation' do
+    before(:each) { FactoryGirl.create_list(:widget, 4) }
+    let(:set) { Widget.all }
 
-        it { should eq [foo, bar, baz] }
+    describe '#process' do
+      subject { processor.process }
+
+      context 'with attributes on the base resource' do
+        context 'when direction is ASC' do
+          let(:sort_structure) { { ship_by: :asc } }
+          let(:expected_result) { Widget.order(ship_by: :asc) }
+
+          it { expect(subject.map(&:id)).to eq expected_result.map(&:id) }
+        end
+
+        context 'when direction is DESC' do
+          let(:sort_structure) { { ship_by: 'desc' } }
+          let(:expected_result) { Widget.order(ship_by: 'desc') }
+
+          it { expect(subject.map(&:id)).to eq expected_result.map(&:id) }
+        end
       end
 
-      context 'when direction is DESC' do
-        let(:sort_structure) { { association: { key: :desc } } }
+      context 'with attributes on an associated resource' do
+        context 'when direction is ASC' do
+          let(:sort_structure) { { customer: { name: 'asc' } } }
+          let(:expected_result) { Widget.joins(:customer).merge(Customer.order(name: 'asc')) }
 
-        it { should eq [baz, bar, foo] }
+          it { expect(subject.map(&:id)).to eq expected_result.map(&:id) }
+        end
+
+        context 'when direction is DESC' do
+          let(:sort_structure) { { customer: { name: :desc } } }
+          let(:expected_result) { Widget.joins(:customer).merge(Customer.order(name: :desc)) }
+
+          it { expect(subject.map(&:id)).to eq expected_result.map(&:id) }
+        end
       end
     end
   end
