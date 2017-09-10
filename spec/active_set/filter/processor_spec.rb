3d963bae7e4ea0fbb9f12ab35f4cd6e8b1af027f
Stephen Margheim <margheim@mail.med.upenn.edu>
2017-09-09 22:48:23 -0400

    Update the Filter spec to test both simple Enumerable set and ActiveRecord::Relation sets

--- spec/active_set/filter/processor_spec.rb
+++ spec/active_set/filter/processor_spec.rb
@@ -4,41 +4,96 @@ require 'spec_helper'
 require 'ostruct'
 
 RSpec.describe ActiveSet::Filter::Processor do
-  let(:foo) { OpenStruct.new(key: 'foo', association: OpenStruct.new(key: 'oof')) }
-  let(:bar) { OpenStruct.new(key: 'bar', association: OpenStruct.new(key: 'rab')) }
-  let(:baz) { OpenStruct.new(key: 'baz', association: OpenStruct.new(key: 'zab')) }
-  let(:set) { [foo, bar, baz] }
-
   let(:processor) { described_class.new(set, filter_structure) }
 
-  describe '#process' do
-    subject { processor.process }
+  context 'when set is simple Enumerable' do
+    let(:foo) { OpenStruct.new(key: 'foo', association: OpenStruct.new(key: 'oof')) }
+    let(:bar) { OpenStruct.new(key: 'bar', association: OpenStruct.new(key: 'rab')) }
+    let(:baz) { OpenStruct.new(key: 'baz', association: OpenStruct.new(key: 'zab')) }
+    let(:set) { [foo, bar, baz] }
 
-    context 'with attributes on the base resource' do
-      context 'when value matches' do
-        let(:filter_structure) { { key: bar.key } }
+    describe '#process' do
+      subject { processor.process }
 
-        it { should eq [bar] }
+      context 'with attributes on the base resource' do
+        context 'when value matches' do
+          let(:filter_structure) { { key: bar.key } }
+
+          it { should eq [bar] }
+        end
+
+        context 'when value doesn\'t match' do
+          let(:filter_structure) { { key: '___' } }
+
+          it { should eq [] }
+        end
       end
 
-      context 'when value doesn\'t match' do
-        let(:filter_structure) { { key: '___' } }
+      context 'with attributes on an associated resource' do
+        context 'when value matches' do
+          let(:filter_structure) { { association: { key: foo.association.key } } }
 
-        it { should eq [] }
+          it { should eq [foo] }
+        end
+
+        context 'when value doesn\'t match' do
+          let(:filter_structure) { { association: { key: '___' } } }
+
+          it { should eq [] }
+        end
+
+        context 'when path doesn\'t make sense' do
+          let(:filter_structure) { { no: { sense: '___' } } }
+
+          it { should eq [] }
+        end
       end
     end
+  end
 
-    context 'with attributes on an associated resource' do
-      context 'when value matches' do
-        let(:filter_structure) { { association: { key: foo.association.key } } }
+  context 'when set is ActiveRecord::Relation' do
+    before(:each) { FactoryGirl.create_list(:widget, 4) }
+    let(:set) { Widget.all }
 
-        it { should eq [foo] }
+    describe '#process' do
+      subject { processor.process }
+
+      context 'with attributes on the base resource' do
+        context 'when value matches' do
+          let(:resource) { set.first }
+          let(:filter_structure) { { color: resource.color } }
+          let(:expected_result) { Widget.where(color: resource.color) }
+
+          it { expect(subject.map(&:id)).to eq expected_result.map(&:id) }
+        end
+
+        context 'when value doesn\'t match' do
+          let(:filter_structure) { { color: '___' } }
+
+          it { should eq [] }
+        end
       end
 
-      context 'when value doesn\'t match' do
-        let(:filter_structure) { { association: { key: '___' } } }
+      context 'with attributes on an associated resource' do
+        context 'when value matches' do
+          let(:association) { set.last.customer }
+          let(:filter_structure) { { customer: { name: association.name } } }
+          let(:expected_result) { Widget.joins(:customer).where(customers: { name: association.name }) }
 
-        it { should eq [] }
+          it { expect(subject.map(&:id)).to eq expected_result.map(&:id) }
+        end
+
+        context 'when value doesn\'t match' do
+          let(:filter_structure) { { customer: { name: '___' } } }
+
+          it { should eq [] }
+        end
+
+        context 'when path doesn\'t make sense' do
+          let(:filter_structure) { { no: { sense: '___' } } }
+
+          it { should eq [] }
+        end
       end
     end
   end
