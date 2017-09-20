670e413104c5a096d7b38e48ff745f39d3e3cd50
Stephen Margheim <margheim@mail.med.upenn.edu>
2017-09-20 17:00:17 -0400

    Don’t cast DateTime and Time object to Integers in the EnumerableAdapter for the Filter Processor
    Let’s play in regular Ruby-land the whole time

--- lib/active_set/processors/filter/enumerable_adapter.rb
+++ lib/active_set/processors/filter/enumerable_adapter.rb
@@ -7,26 +7,10 @@ class ActiveSet
     class EnumerableAdapter < BaseAdapter
       def process(set)
         set.select do |item|
-          value_for(item).send(@instruction.operator,
-                               passed_value)
+          @instruction.value_for(item: item)
+                      .send(@instruction.operator,
+                            @instruction.value)
         end
-      end
-
-      private
-
-      def value_for(item)
-        convert_datetime_values_to_integer(@instruction.value_for(item: item))
-      end
-
-      def passed_value
-        convert_datetime_values_to_integer(@instruction.value)
-      end
-
-      def convert_datetime_values_to_integer(value)
-        # DateTimes (and Times) are tricky to compare, Integers are easier
-        # and I only care about second precision
-        return value unless value.is_a?(DateTime)
-        value.to_i
       end
     end
   end
