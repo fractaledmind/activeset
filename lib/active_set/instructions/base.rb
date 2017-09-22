ebd9f005ee7fcefcee518b24d3e54e657631c6d2
Stephen Margheim <margheim@mail.med.upenn.edu>
2017-09-22 10:42:18 -0400

    Only run an Adapter for a Process if the previous Adapter wasn’t able to process the set
    So, Adapters are put in a queue for the Processor. The processor iterates over the adapters, processing the set with each one in turn. That processing will then iterate over the instructions, processing each one of those in turn.
    
    Previously, every Adapter was run with every instruction. Now, if the first Adapter can handle the instruction, no later adapters will try to process that instruction. If the first Adapter _cannot_ handle the instruction, it will bail and the next Adapter can try.

--- lib/active_set/instructions/base.rb
+++ lib/active_set/instructions/base.rb
@@ -8,18 +8,27 @@ class ActiveSet
 
     def initialize(hash)
       @hash = hash
-      @flattened_hash = hash.flatten_keys
+      @flattened_hash = hash.flatten_keys.transform_keys { |k| k.map(&:to_s) }
     end
 
     def process_adapter(set:, adapter:)
       @flattened_hash.reduce(set) do |inner_set, (keypath, value)|
         instruction = Entry.new(keypath, value)
-        adapter.new(instruction).process(inner_set)
+        output = adapter.new(inner_set, instruction).process
+        remove_from_instruction_set(key: instruction.path) if output[:processed]
+
+        output[:set]
       end
     end
 
     def get(keypath)
       @hash.dig(*keypath)
+    end
+
+    private
+
+    def remove_from_instruction_set(key:)
+      @flattened_hash.delete(key)
     end
   end
 end
