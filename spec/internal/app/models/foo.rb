ece08943a0934391b761829dd744d3123b6d21c3
Stephen Margheim <margheim@mail.med.upenn.edu>
2017-10-12 10:43:50 -0400

    Test scopes defined using the helper but also as simply class methods

--- spec/internal/app/models/foo.rb
+++ spec/internal/app/models/foo.rb
@@ -3,11 +3,11 @@
 class Foo < ApplicationRecord
   belongs_to :assoc
 
-  scope :datetime_between, (lambda do |start, finish|
+  scope :datetime_between, (lambda do |(start, finish)|
     where(Arel::Table.new(table_name)[:datetime].between(start..finish))
   end)
 
-  scope :string_starts_with, (lambda do |substr|
+  def self.string_starts_with(substr)
     where(Arel::Table.new(table_name)[:string].matches("#{substr}%"))
-  end)
+  end
 end
