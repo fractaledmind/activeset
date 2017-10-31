7aa4fd29d7999623de04b0c11b45fcbb182d2474
Stephen Margheim <margheim@mail.med.upenn.edu>
2017-10-31 11:36:17 -0400

    Beef up the PaginateProcessor spec

--- spec/active_set/paginate/processor_spec.rb
+++ spec/active_set/paginate/processor_spec.rb
@@ -4,30 +4,70 @@ require 'spec_helper'
 require 'ostruct'
 
 RSpec.describe ActiveSet::PaginateProcessor do
-  let(:foo) { OpenStruct.new(key: 'foo', association: OpenStruct.new(key: 'oof')) }
-  let(:bar) { OpenStruct.new(key: 'bar', association: OpenStruct.new(key: 'rab')) }
-  let(:baz) { OpenStruct.new(key: 'baz', association: OpenStruct.new(key: 'zab')) }
-  let(:set) { [foo, bar, baz] }
-
   let(:processor) { described_class.new(set, paginate_structure) }
 
-  describe '#process' do
-    subject { processor.process }
+  context 'when set is Enumerable and instructions hash has Symbol keys' do
+    let(:foo) { OpenStruct.new(key: 'foo', association: OpenStruct.new(key: 'oof')) }
+    let(:bar) { OpenStruct.new(key: 'bar', association: OpenStruct.new(key: 'rab')) }
+    let(:baz) { OpenStruct.new(key: 'baz', association: OpenStruct.new(key: 'zab')) }
+    let(:set) { [foo, bar, baz] }
 
-    context 'when page size is smaller than set size' do
-      context 'when set is divisible by page size' do
-        let(:size) { 1 }
+    describe '#process' do
+      subject { processor.process }
 
-        context 'when on first page' do
-          let(:paginate_structure) { { page: 1, size: size } }
+      context 'when page size is smaller than set size' do
+        context 'when set is divisible by page size' do
+          let(:size) { 1 }
 
-          it { should eq [foo] }
+          context 'when on first page' do
+            let(:paginate_structure) { { page: 1, size: size } }
+
+            it { should eq [foo] }
+          end
+
+          context 'when on last page' do
+            let(:paginate_structure) { { page: 3, size: size } }
+
+            it { should eq [baz] }
+          end
+
+          context 'when on irrational page' do
+            let(:paginate_structure) { { page: 10, size: size } }
+
+            it { should eq [] }
+          end
         end
 
-        context 'when on last page' do
-          let(:paginate_structure) { { page: 3, size: size } }
+        context 'when set is not divisible by page size' do
+          let(:size) { 2 }
 
-          it { should eq [baz] }
+          context 'when on first page' do
+            let(:paginate_structure) { { page: 1, size: size } }
+
+            it { should eq [foo, bar] }
+          end
+
+          context 'when on last page' do
+            let(:paginate_structure) { { page: 2, size: size } }
+
+            it { should eq [baz] }
+          end
+
+          context 'when on irrational page' do
+            let(:paginate_structure) { { page: 10, size: size } }
+
+            it { should eq [] }
+          end
+        end
+      end
+
+      context 'when page size is equal to set size' do
+        let(:size) { set.count }
+
+        context 'when on only page' do
+          let(:paginate_structure) { { page: 1, size: size } }
+
+          it { should eq [foo, bar, baz] }
         end
 
         context 'when on irrational page' do
@@ -37,19 +77,13 @@ RSpec.describe ActiveSet::PaginateProcessor do
         end
       end
 
-      context 'when set is not divisible by page size' do
-        let(:size) { 2 }
+      context 'when page size is greater than set size' do
+        let(:size) { set.count + 1 }
 
-        context 'when on first page' do
+        context 'when on only page' do
           let(:paginate_structure) { { page: 1, size: size } }
 
-          it { should eq [foo, bar] }
-        end
-
-        context 'when on last page' do
-          let(:paginate_structure) { { page: 2, size: size } }
-
-          it { should eq [baz] }
+          it { should eq [foo, bar, baz] }
         end
 
         context 'when on irrational page' do
@@ -59,36 +93,93 @@ RSpec.describe ActiveSet::PaginateProcessor do
         end
       end
     end
+  end
 
-    context 'when page size is equal to set size' do
-      let(:size) { set.count }
+  context 'when set is ActiveRecord::Relation and instructions hash has String keys' do
+    include_context 'for active record sets'
+    let!(:baz) { Foo.create(assoc: Assoc.create) }
 
-      context 'when on only page' do
-        let(:paginate_structure) { { page: 1, size: size } }
+    let(:set) { Foo.all }
 
-        it { should eq [foo, bar, baz] }
+    describe '#process' do
+      subject { processor.process }
+
+      context 'when page size is smaller than set size' do
+        context 'when set is divisible by page size' do
+          let(:size) { 1 }
+
+          context 'when on first page' do
+            let(:paginate_structure) { { 'page' => 1, 'size' => size } }
+
+            it { should eq [foo] }
+          end
+
+          context 'when on last page' do
+            let(:paginate_structure) { { 'page' => 3, 'size' => size } }
+
+            it { should eq [baz] }
+          end
+
+          context 'when on irrational page' do
+            let(:paginate_structure) { { 'page' => 10, 'size' => size } }
+
+            it { should eq [] }
+          end
+        end
+
+        context 'when set is not divisible by page size' do
+          let(:size) { 2 }
+
+          context 'when on first page' do
+            let(:paginate_structure) { { 'page' => 1, 'size' => size } }
+
+            it { should eq [foo, bar] }
+          end
+
+          context 'when on last page' do
+            let(:paginate_structure) { { 'page' => 2, 'size' => size } }
+
+            it { should eq [baz] }
+          end
+
+          context 'when on irrational page' do
+            let(:paginate_structure) { { 'page' => 10, 'size' => size } }
+
+            it { should eq [] }
+          end
+        end
       end
 
-      context 'when on irrational page' do
-        let(:paginate_structure) { { page: 10, size: size } }
+      context 'when page size is equal to set size' do
+        let(:size) { set.count }
 
-        it { should eq [] }
+        context 'when on only page' do
+          let(:paginate_structure) { { 'page' => 1, 'size' => size } }
+
+          it { should eq [foo, bar, baz] }
+        end
+
+        context 'when on irrational page' do
+          let(:paginate_structure) { { 'page' => 10, 'size' => size } }
+
+          it { should eq [] }
+        end
       end
-    end
 
-    context 'when page size is greater than set size' do
-      let(:size) { set.count + 1 }
+      context 'when page size is greater than set size' do
+        let(:size) { set.count + 1 }
 
-      context 'when on only page' do
-        let(:paginate_structure) { { page: 1, size: size } }
+        context 'when on only page' do
+          let(:paginate_structure) { { 'page' => 1, 'size' => size } }
 
-        it { should eq [foo, bar, baz] }
-      end
+          it { should eq [foo, bar, baz] }
+        end
 
-      context 'when on irrational page' do
-        let(:paginate_structure) { { page: 10, size: size } }
+        context 'when on irrational page' do
+          let(:paginate_structure) { { 'page' => 10, 'size' => size } }
 
-        it { should eq [] }
+          it { should eq [] }
+        end
       end
     end
   end
