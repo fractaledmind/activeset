c524f41c5d1723cc4ee2d1d3b3d7fb61d1f1b70e
Stephen Margheim <margheim@mail.med.upenn.edu>
2017-12-21 14:18:50 -0500

    Add tests for the total_count resetting behavior

--- spec/active_set_spec.rb
+++ spec/active_set_spec.rb
@@ -20,4 +20,37 @@ RSpec.describe ActiveSet do
       expect(ActiveSet.new([])).to respond_to :each
     end
   end
+
+  context '#filter' do
+    let(:set) { [1, 2, 3] }
+    let(:active_set) { ActiveSet.new(set) }
+    let(:filtered_set) { active_set.filter(filter_instructions) }
+    let(:filter_instructions) { { itself: 1 } }
+
+    it 'resets the set\'s total_count' do
+      expect(filtered_set.total_count).to eq 1
+    end
+  end
+
+  context '#sort' do
+    let(:set) { [1, 2, 3] }
+    let(:active_set) { ActiveSet.new(set) }
+    let(:sorted_set) { active_set.sort(sort_instructions) }
+    let(:sort_instructions) { { itself: :asc } }
+
+    it 'does not reset the set\'s total_count' do
+      expect(sorted_set.total_count).to eq set.count
+    end
+  end
+
+  context '#paginate' do
+    let(:set) { [1, 2, 3] }
+    let(:active_set) { ActiveSet.new(set) }
+    let(:paginated_set) { active_set.paginate(paginate_instructions) }
+    let(:paginate_instructions) { { page: 1, size: 1 } }
+
+    it 'does not reset the set\'s total_count' do
+      expect(paginated_set.total_count).to eq set.count
+    end
+  end
 end
