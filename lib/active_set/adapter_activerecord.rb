e6032d0e04f8ac0d3a5ffd0d7414a630758bee9a
Stephen Margheim <margheim@mail.med.upenn.edu>
2018-08-05 14:05:27 -0400

    Add some ActiveRecord adapter common methods for working with scopes and properly eager loading associations

--- lib/active_set/adapter_activerecord.rb
+++ lib/active_set/adapter_activerecord.rb
@@ -6,8 +6,17 @@ class ActiveSet
   class Adapter::ActiveRecord < Adapter::Base
     private
 
-    def can_process_with_active_record?
-      @set.respond_to?(:to_sql) && attribute_is_field?
+    def can_query_with_active_record?
+      attribute_is_field?
+    end
+
+    def can_merge_with_active_record?
+      attribute_is_class_method? &&
+        attribute_is_method_with_params?
+    end
+
+    def arel_eager_load_associations
+      @set.eager_load(instruction.associations_hash)
     end
 
     def arel_column
@@ -25,11 +34,25 @@ class ActiveSet
                      .include?(instruction.attribute)
     end
 
+    def attribute_is_class_method?
+      return false unless attribute_model
+
+      attribute_model.respond_to?(instruction.attribute)
+    end
+
+    def attribute_is_method_with_params?
+      return false unless attribute_model
+
+      attribute_model.method(instruction.attribute).arity != 0
+    end
+
     def attribute_model
-      instruction.associations_array
-                  .reduce(@set) do |obj, assoc|
-                    obj.reflections[assoc.to_s]&.klass
-                  end
+      tmp_model = instruction.associations_array
+                             .reduce(@set) do |obj, assoc|
+                               obj.reflections[assoc.to_s]&.klass
+                             end
+      # return tmp_model.klass if tmp_model.is_a?(ActiveRecord::Relation)
+      # tmp_model
     end
   end
 end
