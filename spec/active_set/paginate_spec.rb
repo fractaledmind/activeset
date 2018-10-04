79ef2f00379e840b4a42caf373572959896a4146
Stephen Margheim <margheim@mail.med.upenn.edu>
2018-10-04 18:12:47 -0400

    Get the pagination specs in order

--- spec/active_set/paginate_spec.rb
+++ spec/active_set/paginate_spec.rb
@@ -2,99 +2,76 @@
 
 require 'spec_helper'
 
-begin
-  FactoryGirl.find_definitions
-rescue FactoryGirl::DuplicateDefinitionError
-end
-
-FOO_PAGINATING_1 = FactoryGirl.create(:foo, assoc: FactoryGirl.create(:assoc))
-FOO_PAGINATING_2 = FactoryGirl.create(:foo, assoc: FactoryGirl.create(:assoc))
-FOO_PAGINATING_3 = FactoryGirl.create(:foo, assoc: FactoryGirl.create(:assoc))
-
-PAGINATING_ACTIVE_SET = ActiveSet.new(Foo.all)
-
 RSpec.describe ActiveSet do
+  before(:all) do
+    @foo_1 = FactoryGirl.create(:foo, assoc: FactoryGirl.create(:assoc))
+    @foo_2 = FactoryGirl.create(:foo, assoc: FactoryGirl.create(:assoc))
+    @foo_3 = FactoryGirl.create(:foo, assoc: FactoryGirl.create(:assoc))
+    @active_set = ActiveSet.new(Foo.all)
+  end
+  after(:all) { Foo.delete_all }
+
   describe '#paginate' do
-    { page: 1, size: 1 }.tap do |instructions|
-      it "#{instructions.inspect}" do
-        result = PAGINATING_ACTIVE_SET.paginate(instructions)
+    let(:result) { @active_set.paginate(instructions) }
 
-        expect(result.map(&:id)).to eq [FOO_PAGINATING_1.id]
+      context '{ page: 1, size: 1 }' do
+        let(:instructions) { { page: 1, size: 1 } }
+
+        it { expect(result.map(&:id)).to eq [@foo_1.id] }
       end
-    end
 
-    { page: 2, size: 1 }.tap do |instructions|
-      it "#{instructions.inspect}" do
-        result = PAGINATING_ACTIVE_SET.paginate(instructions)
+      context '{ page: 2, size: 1 }' do
+        let(:instructions) { { page: 2, size: 1 } }
 
-        expect(result.map(&:id)).to eq [FOO_PAGINATING_2.id]
+        it { expect(result.map(&:id)).to eq [@foo_2.id] }
       end
-    end
 
-    { page: 10, size: 1 }.tap do |instructions|
-      it "#{instructions.inspect}" do
-        result = PAGINATING_ACTIVE_SET.paginate(instructions)
+      context '{ page: 10, size: 1 }' do
+        let(:instructions) { { page: 10, size: 1 } }
 
-        expect(result.map(&:id)).to eq []
+        it { expect(result.map(&:id)).to eq [] }
       end
-    end
 
-    { page: 1, size: 2 }.tap do |instructions|
-      it "#{instructions.inspect}" do
-        result = PAGINATING_ACTIVE_SET.paginate(instructions)
+      context '{ page: 1, size: 2 }' do
+        let(:instructions) { { page: 1, size: 2 } }
 
-        expect(result.map(&:id)).to eq [FOO_PAGINATING_1.id, FOO_PAGINATING_2.id]
+        it { expect(result.map(&:id)).to eq [@foo_1.id, @foo_2.id] }
       end
-    end
 
-    { page: 2, size: 2 }.tap do |instructions|
-      it "#{instructions.inspect}" do
-        result = PAGINATING_ACTIVE_SET.paginate(instructions)
+      context '{ page: 2, size: 2 }' do
+        let(:instructions) { { page: 2, size: 2 } }
 
-        expect(result.map(&:id)).to eq [FOO_PAGINATING_3.id]
+        it { expect(result.map(&:id)).to eq [@foo_3.id] }
       end
-    end
 
-    { page: 10, size: 2 }.tap do |instructions|
-      it "#{instructions.inspect}" do
-        result = PAGINATING_ACTIVE_SET.paginate(instructions)
+      context '{ page: 10, size: 2 }' do
+        let(:instructions) { { page: 10, size: 2 } }
 
-        expect(result.map(&:id)).to eq []
+        it { expect(result.map(&:id)).to eq [] }
       end
-    end
 
-    { page: 1, size: 3 }.tap do |instructions|
-      it "#{instructions.inspect}" do
-        result = PAGINATING_ACTIVE_SET.paginate(instructions)
+      context '{ page: 1, size: 3 }' do
+        let(:instructions) { { page: 1, size: 3 } }
 
-        expect(result.map(&:id)).to eq [FOO_PAGINATING_1.id, FOO_PAGINATING_2.id, FOO_PAGINATING_3.id]
+        it { expect(result.map(&:id)).to eq [@foo_1.id, @foo_2.id, @foo_3.id] }
       end
-    end
 
-    { page: 10, size: 3 }.tap do |instructions|
-      it "#{instructions.inspect}" do
-        result = PAGINATING_ACTIVE_SET.paginate(instructions)
+      context '{ page: 10, size: 3 }' do
+        let(:instructions) { { page: 10, size: 3 } }
 
-        expect(result.map(&:id)).to eq []
+        it { expect(result.map(&:id)).to eq [] }
       end
-    end
 
-    { page: 1, size: 5 }.tap do |instructions|
-      it "#{instructions.inspect}" do
-        result = PAGINATING_ACTIVE_SET.paginate(instructions)
+      context '{ page: 1, size: 5 }' do
+        let(:instructions) { { page: 1, size: 5 } }
 
-        expect(result.map(&:id)).to eq [FOO_PAGINATING_1.id, FOO_PAGINATING_2.id, FOO_PAGINATING_3.id]
+        it { expect(result.map(&:id)).to eq [@foo_1.id, @foo_2.id, @foo_3.id] }
       end
-    end
 
-    { page: 10, size: 5 }.tap do |instructions|
-      it "#{instructions.inspect}" do
-        result = PAGINATING_ACTIVE_SET.paginate(instructions)
+      context '{ page: 10, size: 5 }' do
+        let(:instructions) { { page: 10, size: 5 } }
 
-        expect(result.map(&:id)).to eq []
+        it { expect(result.map(&:id)).to eq [] }
       end
-    end
   end
 end
-
-Foo.destroy_all
