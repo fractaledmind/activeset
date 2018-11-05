d34bc4eaeb7d498b3cdf14208d3fd751e78da93e
Stephen Margheim <margheim@mail.med.upenn.edu>
2018-11-05 17:25:11 -0500

    Add a couple more tests for the transform_to_sortable_numeric method

--- spec/helpers/transform_to_sortable_numeric_spec.rb
+++ spec/helpers/transform_to_sortable_numeric_spec.rb
@@ -5,69 +5,81 @@ require 'spec_helper'
 RSpec.describe '#transform_to_sortable_numeric' do
   let(:result) { transform_to_sortable_numeric(value) }
 
-  context 'Integer' do
-    let(:value) { rand(10) }
+  describe 'converts type to numeric' do
+    context 'Integer' do
+      let(:value) { rand(10) }
 
-    it { expect(result).to eq value }
+      it { expect(result).to eq value }
+    end
+
+    context 'Float' do
+      let(:value) { rand() }
+
+      it { expect(result).to eq value }
+    end
+
+    context 'Complex' do
+      let(:value) { Complex(rand(10), rand(10)) }
+
+      it { expect(result).to eq value }
+    end
+
+    context 'Rational' do
+      let(:value) { Rational(rand(10), rand(10) + 1) }
+
+      it { expect(result).to eq value }
+    end
+
+    context 'TrueClass' do
+      let(:value) { true }
+
+      it { expect(result).to eq 1 }
+    end
+
+    context 'FalseClass' do
+      let(:value) { false }
+
+      it { expect(result).to eq 0 }
+    end
+
+    context 'String' do
+      let(:value) { 'aB09ü' }
+
+      it { expect(result).to eq Rational(24266512014313, 250000000000) }
+    end
+
+    context 'Symbol' do
+      let(:value) { 'aB09ü'.to_sym }
+
+      it { expect(result).to eq Rational(24266512014313, 250000000000) }
+    end
+
+    context 'Date' do
+      let(:value) { Date.new(2000, 12, 25) }
+
+      it { expect(result).to eq 977720400000 }
+    end
+
+    context 'Time' do
+      let(:value) { Time.new(2000, 12, 25, 11, 30) }
+
+      it { expect(result).to eq 977761800000 }
+    end
+
+    context 'DateTime' do
+      let(:value) { DateTime.new(2000, 12, 25, 11, 30) }
+
+      it { expect(result).to eq 977743800000 }
+    end
   end
 
-  context 'Float' do
-    let(:value) { rand() }
+  describe 'retains sortability' do
+    let(:sorted_array) { array.sort_by { |x| transform_to_sortable_numeric(x) } }
 
-    it { expect(result).to eq value }
-  end
+    context 'array of Strings' do
+      let(:array) { %w[a z A Z] }
 
-  context 'Complex' do
-    let(:value) { Complex(rand(10), rand(10)) }
-
-    it { expect(result).to eq value }
-  end
-
-  context 'Rational' do
-    let(:value) { Rational(rand(10), rand(10) + 1) }
-
-    it { expect(result).to eq value }
-  end
-
-  context 'TrueClass' do
-    let(:value) { true }
-
-    it { expect(result).to eq 1 }
-  end
-
-  context 'FalseClass' do
-    let(:value) { false }
-
-    it { expect(result).to eq 0 }
-  end
-
-  context 'String' do
-    let(:value) { 'aB09ü' }
-
-    it { expect(result).to eq Rational(24266512014313, 250000000000) }
-  end
-
-  context 'Symbol' do
-    let(:value) { 'aB09ü'.to_sym }
-
-    it { expect(result).to eq Rational(24266512014313, 250000000000) }
-  end
-
-  context 'Date' do
-    let(:value) { Date.new(2000, 12, 25) }
-
-    it { expect(result).to eq 977720400000 }
-  end
-
-  context 'Time' do
-    let(:value) { Time.new(2000, 12, 25, 11, 30) }
-
-    it { expect(result).to eq 977761800000 }
-  end
-
-  context 'DateTime' do
-    let(:value) { DateTime.new(2000, 12, 25, 11, 30) }
-
-    it { expect(result).to eq 977743800000 }
+      it { expect(sorted_array).to eq array.sort }
+    end
   end
 end
