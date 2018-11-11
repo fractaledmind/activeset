456863e7700b30969d7f3d6029abf52b44d456c4
Stephen Margheim <margheim@mail.med.upenn.edu>
2018-11-11 10:49:39 -0500

    Update the `flatten_keys` Hash extension to handle hashes with array values
    Add some basic tests for this new functionality as well

--- lib/patches/core_ext/hash/flatten_keys.rb
+++ lib/patches/core_ext/hash/flatten_keys.rb
@@ -8,8 +8,6 @@ class Hash
   #   hash = { person: { name: { first: 'Rob' }, age: '28' } }
   #   hash.flatten_keys_to_array
   #   => {[:person, :name, :first] => "Rob", [:person, :age]=>"28" }
-  #   hash
-  #   => { person: { name: { first: 'Rob' }, age: '28' } }
   def flatten_keys_to_array
     _flatten_keys(self)
   end
@@ -19,9 +17,7 @@ class Hash
   #
   #   hash = { person: { name: { first: 'Rob' }, age: '28' } }
   #   hash.flatten_keys_to_dotpath
-  #   => { 'person.name.first' => "Rob", [:person, :age]=>"28" }
-  #   hash
-  #   => { person: { name: { first: 'Rob' }, age: '28' } }
+  #   => { 'person.name.first' => "Rob", 'person.age'=>"28" }
   def flatten_keys_to_dotpath
     _flatten_keys(self, ->(*keys) { keys.join('.') })
   end
@@ -30,9 +26,7 @@ class Hash
   #
   #   hash = { person: { name: { first: 'Rob' }, age: '28' } }
   #   hash.flatten_keys_to_html_attribute
-  #   => { 'person-name-first' => "Rob", [:person, :age]=>"28" }
-  #   hash
-  #   => { person: { name: { first: 'Rob' }, age: '28' } }
+  #   => { 'person-name-first' => "Rob", 'person-age'=>"28" }
   def flatten_keys_to_html_attribute
     _flatten_keys(self, ->(*keys) { keys.join('-') })
   end
@@ -41,20 +35,22 @@ class Hash
   #
   #   hash = { person: { name: { first: 'Rob' }, age: '28' } }
   #   hash.flatten_keys_to_rails_param
-  #   => { 'person[name][first]' => "Rob", [:person, :age]=>"28" }
-  #   hash
-  #   => { person: { name: { first: 'Rob' }, age: '28' } }
+  #   => { 'person[name][first]' => "Rob", 'person[age]'=>"28" }
   def flatten_keys_to_rails_param
     _flatten_keys(self, ->(*keys) { keys.map(&:to_s).reduce { |memo, key| memo + "[#{key}]" } })
   end
 
   private
 
-  # refactored little from https://stackoverflow.com/a/23861946/2884386
+  # refactored from https://stackoverflow.com/a/23861946/2884386
   def _flatten_keys(input, keypath_gen = ->(*keys) { keys }, keys = [], output = {})
-    return output.merge!(keypath_gen.call(*keys) => input) unless input.is_a? Hash
-
-    input.each { |k, v| _flatten_keys(v, keypath_gen, keys + Array(k), output) }
+    if input.is_a? Hash
+      input.each { |k, v| _flatten_keys(v, keypath_gen, keys + Array(k), output) }
+    elsif input.is_a? Array
+      input.each_with_index { |v, i| _flatten_keys(v, keypath_gen, keys + Array(i), output) }
+    else
+      return output.merge!(keypath_gen.call(*keys) => input)
+    end
 
     output
   end
