cf6bfa48ab76aeeac9ac22a26178967bf721ea67
fractaledmind <stephen.margheim@gmail.com>
2019-05-05 18:38:29 +0200

    Update the all_possible_paths_for spec helper method to be more controllable

--- spec/support/path_helpers.rb
+++ spec/support/path_helpers.rb
@@ -1,13 +1,21 @@
 module PathHelpers
-  def all_possible_paths_for(type)
-    %W[
-      #{type}
-      computed_#{type}
-      one.#{type}
-      one.computed_#{type}
-      computed_one.#{type}
-      computed_one.computed_#{type}
-    ]
+  def all_possible_paths_for(type, options = {})
+    association = 'one'
+    computed_field = "computed_#{type}"
+    computed_association = "computed_#{association}"
+
+    [].tap do |paths|
+      paths << type
+      paths << computed_field unless options[:computed_fields] == false
+      unless options[:associations] == false
+        paths << [association, type].join('.')
+        paths << [association, computed_field].join('.') unless options[:computed_fields] == false
+      end
+      unless options[:computed_associations] == false
+        paths << [computed_association, type].join('.')
+        paths << [computed_association, computed_field].join('.') unless options[:computed_fields] == false
+      end
+    end
   end
 
   def all_possible_path_combinations_for(type_1, type_2)
